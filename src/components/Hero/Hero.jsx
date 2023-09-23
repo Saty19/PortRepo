ef826f9d5e7868635cf32c762e80693e4bd564f9
Satyadarsan senapati <141367726+Saty19@users.noreply.github.com>
2023-09-23 17:19:03 +0530

    push

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from 'react';
+import  { useEffect, useState } from 'react';
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
+import { FaFacebookSquare } from 'react-icons/fa';
 import gsap from 'gsap';
 import styles from './Hero.module.css'; // Import your CSS module here
 import video from './video.webm';
 
 const Hero = () => {
+
+
+
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  // Function to update screen width when the window is resized
+  const updateScreenWidth = () => {
+    setScreenWidth(window.innerWidth);
+  };
+
   useEffect(() => {
     const card = document.querySelector(`.${styles.card}`);
-    const topSection = document.querySelector(`.${styles.container}`);
+    const topSection = document.querySelector(".can");
 
     topSection.addEventListener('mousemove', (event) => {
-      const mouseX = -((window.innerWidth / 2) - event.pageX) / 30;
-      const mouseY = ((window.innerHeight / 2) - event.pageY) / 30;
+      const mouseX = -((window.innerWidth / 2) - event.pageX) / 60;
+      const mouseY = ((window.innerHeight / 2) - event.pageY) / 60;
 
       gsap.to(card, {
         duration: 0.5,
@@ -19,7 +31,12 @@ const Hero = () => {
         ease: 'power2.out',
       });
     });
+    window.addEventListener('resize', updateScreenWidth);
+    return () => {
+      window.removeEventListener('resize', updateScreenWidth);
+    };
   }, []);
+
 
   return (
     <div className={`can ${styles.container}`}>
@@ -35,14 +52,22 @@ const Hero = () => {
       </div>
 
       <div className={styles.MainContent}>
-        <div className={`${styles.card} card`}>
-          <div className={styles.thumb}></div>
-          <h2>Gsap</h2>
-          <span>3D card Animation on hover!</span>
-        </div>
+
+      <div className={`${styles.card} card`}>
+        <div className={`${styles.thumb} thumb`}></div>
+        <h2 className={`${styles.Cardh1} Cardh1`}>SATYA</h2>
+        <span className={`${styles.span} span`}>CREATIVE THE WAY YOU KNOW!</span>
+      </div>
+      
+      
       </div>
 
-      <div className={styles.linkTag}></div>
+      <div className={styles.linkTag}>
+        <FaFacebookSquare color={screenWidth>840?"white":"black"}/>
+        <AiFillGithub color={screenWidth>840?"white":"black"}/>
+        <AiFillInstagram color={screenWidth>840?"white":"black"}/>
+        
+        </div>
     </div>
   );
 };
--- src/components/Product/PhysicsDemo.jsx
+++ src/components/Product/PhysicsDemo.jsx
@@ -1,12 +1,103 @@
-import React from 'react'
-import "./PhysicsDemo.css"
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "./PhysicsDemo.css";
+import img from "./img.jpg";
 
-const PhysicsDemo = () => {
+// Initialize ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+function PhysicsDemo() {
+  const textRef = useRef(null);
+  const container = useRef(null);
+
+  useEffect(() => {
+    function animation_text_1(element) {
+      const newText = [];
+      const theText = textRef.current;
+
+      for (let i = 0; i < theText.innerText.length; i++) {
+        const newChar = document.createElement("div");
+
+        if (theText.innerText[i] === " ") {
+          newChar.innerHTML = "&nbsp;";
+        } else {
+          newChar.textContent = theText.innerText[i];
+        }
+
+        newText.push(newChar);
+      }
+
+      theText.innerHTML = "";
+
+      newText.forEach((char) => {
+        theText.appendChild(char);
+      });
+
+      gsap.fromTo(
+        `${element} div`,
+        {
+          opacity: 0,
+          y: 30,
+        },
+        {
+          duration: 1,
+          opacity: 1,
+          y: 0,
+          stagger: 0.02,
+          // ease: 'elastic(1.2, 0.5)',
+          scrollTrigger: {
+            trigger: element,
+            start: "top 70%",
+            toggleActions: "play pause none none",
+            markers: true,
+            pinSpacing: true,
+            // scrub:0.5
+          },
+        },
+        0
+      );
+    }
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: container.current,
+        start: "10% 80%",
+        end: "90% 10%",
+        scrub: 0.8,
+      },
+    });
+    tl.to(".box", { width: "40%", duration: 1 }, 0.2);
+    // Call the animation function with your target element
+    animation_text_1("#text-anim");
+  }, []);
+
   return (
-    <div className='container'>
-      milan
+    <div className="container" ref={container}>
+      <h1 ref={textRef} id="text-anim">
+        NOW I AM JUST USING THIS FOR TESTING
+      </h1>
+      <div
+        style={{
+          width: "80%",
+          height: "1px",
+          background: "black",
+          float: "right",
+          marginTop: "10%",
+          marginLeft: "auto",
+        }}
+      />
+      <div className="boxWrapper">
+        <div className="BoxWrapperContent">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem iusto
+          nulla pariatur sunt suscipit non sit fugiat, optio repellendus
+          commodi?
+        </div>
+        <div className="box">
+          <img src={img} className="img" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default PhysicsDemo
+export default PhysicsDemo;
