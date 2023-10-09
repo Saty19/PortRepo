9395baca5c4ba79ad65e35e1bb2743323a6456fc
Satyadarsan senapati <141367726+Saty19@users.noreply.github.com>
2023-10-10 01:00:04 +0530

    push

--- src/components/Section2/SectionSecond.jsx
+++ src/components/Section2/SectionSecond.jsx
@@ -1,7 +1,8 @@
-import style from "./Home.module.css";
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useEffect, useRef, useMemo } from 'react';
+import { useEffect, useRef, useMemo } from "react";
+import style from "./SecSecond.module.css";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import gsap from "gsap";
+import image from "/SecImage.jpg";
 
 const SectionSecond = () => {
   const rec1 = useRef(null);
@@ -9,16 +10,14 @@ const SectionSecond = () => {
   const rec3 = useRef(null);
   const rec4 = useRef(null);
   const imgPrlx = useRef(null);
+
   const rightContent = useRef(null);
   const leftContent = useRef(null);
   const parra = useRef(null);
   const leftTop = useRef(null);
   const ThreeContent = useRef(null);
 
-  const imageUrl = "https://plus.unsplash.com/premium_photo-1674740443999-3d67127b5389?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fDNkfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60";
-
-  // Memoize the animation function
-  const animationTimeline = useMemo(() => {
+  useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
     const isMobile = window.matchMedia("(max-width: 820px)").matches;
@@ -37,66 +36,114 @@ const SectionSecond = () => {
     tl.to(rec2.current, { y: "100%", direction: 1 }, 2.5);
     tl.to(rec3.current, { y: "100%", direction: 1 }, 3);
     tl.to(rec4.current, { y: "100%", direction: 1 }, 3.5);
-    tl.to(parra.current, { opacity: 1, duration: 3, position: isMobile && "absolute", zIndex: isMobile && 2 }, 3);
-    tl.to(ThreeContent.current, { width: "70%", top: isMobile ? "70%" : "80%", height: isMobile && "70%", borderRadius: 10, duration: 2 }, 4);
-    tl.to(rightContent.current, {
-      opacity: 1,
-      duration: 0.8,
-      ease: "none",
-    }, 0.6);
-    tl.to(leftContent.current, {
-      opacity: 1,
-      duration: 1,
-      ease: "none",
-    }, 0);
+    tl.to(
+      parra.current,
+      {
+        opacity: 1,
+        duration: 3,
+        position: isMobile && "absolute",
+        zIndex: isMobile && 2,
+      },
+      3
+    );
+    tl.to(
+      ThreeContent.current,
+      {
+        width: "70%",
+        top: isMobile ? "70%" : "80%",
+        height: isMobile && "70%",
+        borderRadius: 10,
+        duration: 2,
+      },
+      4
+    );
+    tl.to(
+      rightContent.current,
+      {
+        opacity: 1,
+        duration: 0.8,
+        ease: "none",
+      },
+      0.6
+    );
+    tl.to(
+      leftContent.current,
+      {
+        opacity: 1,
+        duration: 1,
+        ease: "none",
+      },
+      0
+    );
 
     gsap.to(imgPrlx.current, {
       y: 0.8 * imgPrlx.current.parentNode.offsetHeight, // Adjust this value as needed
-      ease: 'none',
+      ease: "none",
       scrollTrigger: {
         trigger: imgPrlx.current,
         start: "top top",
         markers: false,
         end: "bottom top ",
         scrub: 0, // Enable scrubbing for parallax effect
-      }
+      },
     });
 
-    return tl;
-  }, []);
-
-  useEffect(() => {
     return () => {
-      animationTimeline.kill(); // Kill the timeline to prevent memory leaks
+      tl.kill(); // Kill the timeline to prevent memory leaks
       ScrollTrigger.getAll().forEach((trigger) => {
         trigger.kill(); // Kill all ScrollTrigger instances
       });
     };
-  }, [animationTimeline]);
+  }, []);
+
+  const imageUrl =
+    "https://plus.unsplash.com/premium_photo-1674740443999-3d67127b5389?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fDNkfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60";
+
+  const elements = useMemo(() => {
+    return ["JAVA", "JAVASCRIPT", "REACT", "THREE"].map((text, index) => (
+      <div className={style.parent} key={index}>
+        <div
+          className={style.rectangle}
+          ref={(el) =>
+            index === 0
+              ? (rec1.current = el)
+              : index === 1
+              ? (rec2.current = el)
+              : index === 2
+              ? (rec3.current = el)
+              : (rec4.current = el)
+          }
+        >
+          {text}
+        </div>
+      </div>
+    ));
+  }, []);
 
   return (
     <div className={`section ${style.container}`}>
-      <img src={imageUrl} ref={imgPrlx} className={style.secimage} style={{ objectFit: "cover" }} />
+      <img
+        src={imageUrl}
+        ref={imgPrlx}
+        className={style.secimage}
+        style={{ objectFit: "cover" }}
+      />
       <div className={style.leftContent} ref={leftContent}>
         <div style={{ width: "100%" }} ref={leftTop}>
-          {["JAVA", "JAVASCRIPT", "REACT", "THREE"].map((text, index) => (
-            <div className={style.parent} key={index}>
-              <div className={style.rectangle} ref={(el) => index === 0 ? rec1.current = el : index === 1 ? rec2.current = el : index === 2 ? rec3.current = el : rec4.current = el}>
-                {text}
-              </div>
-            </div>
-          ))}
+          {elements}
         </div>
+        {/*<p ref={parra} className={style.Parragraph}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis laborum molestiae placeat voluptatum animi at ex quos doloribus mollitia adipisci?
+          </p>*/}
       </div>
+
       <div className={style.rightContent} ref={rightContent}>
         <div className={style.threeContent} ref={ThreeContent}>
           NO LIMIT TO SHOW <br />
           YOUR 3D CONTENT
         </div>
       </div>
-      <div className={style.animatedContent}>
-        BLOW MIND
-      </div>
+      <div className={style.animatedContent}>BLOW MIND</div>
     </div>
   );
 };
--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,4 +1,4 @@
-import  { useEffect, useRef, useState, useMemo } from "react";
+import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import style from "./Service.module.css";
@@ -13,33 +13,25 @@ function Service() {
 
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
-  // Memoize the updateScreenWidth function
-  const updateScreenWidth = useMemo(() => {
-    return () => {
-      setScreenWidth(window.innerWidth);
-    };
-  }, []);
+  // Function to update screen width when the window is resized
+  const updateScreenWidth = () => {
+    setScreenWidth(window.innerWidth);
+  };
 
-  // Memoize the animation timeline
-  const animationTimeline = useMemo(() => {
-    return gsap.timeline({
+  useEffect(() => {
+    gsap.to(container.current, {
+      y: 0.05 * container.current.parentNode.offsetHeight, // Adjust this value as needed
+      ease: 'none',
       scrollTrigger: {
         trigger: container.current,
         start: "top top",
         markers: false,
         end: "bottom top ",
-        scrub: 0,
+        scrub: 0, // Enable scrubbing for parallax effect
       }
     });
-  }, [container]);
 
-  useEffect(() => {
-    animationTimeline.to(container.current, {
-      y: 0.05 * container.current.parentNode.offsetHeight,
-      ease: 'none',
-    });
-
-    animationTimeline.fromTo(
+    gsap.fromTo(
       ".img",
       {
         opacity: 0,
@@ -52,7 +44,8 @@ function Service() {
       { opacity: 1, duration: 4 }
     );
 
-    const tl = animationTimeline.timeline({
+    // Main timeline
+    const tl = gsap.timeline({
       scrollTrigger: {
         trigger: container.current,
         start: "10% 80%",
@@ -60,13 +53,15 @@ function Service() {
         scrub: 2,
       },
     });
-
-    tl.to(".box", {
-      width: screenWidth < 840 ? "90%" : "50%",
-      duration: 3,
-      ease: "ease",
-    }, 1);
-
+    tl.to(
+      ".box",
+      {
+        width: screenWidth < 840 ? "90%" : "50%",
+        duration: 3,
+        ease: "ease",
+      },
+      1
+    );
     tl.fromTo(
       ".BoxWrapperContent",
       { opacity: 0 },
@@ -78,18 +73,18 @@ function Service() {
       2
     );
 
-    animationTimeline.to(textRef.current, {
+    gsap.to(textRef.current, {
       scrollTrigger: {
         trigger: container.current,
         start: `${screenWidth < 840 ? "10%" : "top"} 80%`,
         end: `${screenWidth < 840 ? "80%" : "50%"} top`,
         markers: false,
         toggleActions: "play none none none",
-        scrub: 10,
+        scrub: 10, // Increase the scrub value to slow down the animation
       },
       xPercent: `${screenWidth < 840 ? -250 : -150}`,
       duration: 5,
-      ease: "linear"
+      ease: "linear", // Increase the duration for a smoother effect
     });
 
     // Event listener for window resize
@@ -99,13 +94,14 @@ function Service() {
       // Remove the event listener on unmount
       window.removeEventListener("resize", updateScreenWidth);
     };
-  }, [screenWidth, updateScreenWidth, animationTimeline]);
+  }, [screenWidth]);
 
   const imageUrl = "https://images.unsplash.com/photo-1695883701435-7bd88f796e05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
 
   return (
     <div className={`${style.container}`} ref={container}>
       <img src={imageUrl} className={`${style.serviceImg}`} alt="Service Background" />
+
       <h1 ref={textRef} className={style.textpup}>
         NOW I AM JUST USING THIS FOR TESTING
       </h1>
