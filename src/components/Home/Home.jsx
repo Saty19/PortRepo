a75b6e262e98cb1e77fe92139f9c8710631f9886
Satyadarsan senapati <141367726+Saty19@users.noreply.github.com>
2023-10-11 01:41:13 +0530

    push

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,9 @@
 import style from "./Home.module.css";
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useEffect, useRef, useMemo } from 'react';
+import { useEffect, useRef, useMemo ,useState} from 'react';
+import { AiFillGithub, AiFillInstagram } from 'react-icons/ai';
+import { FaFacebookSquare } from 'react-icons/fa';
 
 const Home = () => {
   const videoUrl = 'https://player.vimeo.com/external/489633226.sd.mp4?s=a500a3f029e29953f06c8ada1ffbb3a2b36b5225&profile_id=164&oauth2_token_id=57447761';
@@ -83,6 +85,41 @@ const Home = () => {
     }
   }, [textAnimation]);
 
+  //screenwidth linktag
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [scrollY, setScrollY] = useState(0);
+  const [showLinkTag, setShowLinkTag] = useState(false);
+
+  // Function to update screen width when the window is resized
+  const updateScreenWidth = () => {
+    setScreenWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    // Add a scroll event listener to track the scroll position
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', updateScreenWidth);
+
+    return () => {
+      // Remove the event listeners when the component unmounts
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', updateScreenWidth);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Calculate the scroll percentage
+    const scrollPercentage = (scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
+
+    // Show the linkTag when scrolling reaches 60%
+    setShowLinkTag(scrollPercentage >= 10);
+  }, [scrollY]);
+
+
   return (
     <div ref={elementRef} className={style.container}>
       <video ref={video} loop autoPlay muted className={`${style.video} video`}>
@@ -96,6 +133,21 @@ const Home = () => {
         </h1>
         <h2 className={style.bottomContent}>VISION</h2>
       </div>
+      {screenWidth > 840 ? (
+        <div className={style.linkTag}>
+          <FaFacebookSquare size={40} color="white" />
+          <AiFillGithub size={40} color="white" />
+          <AiFillInstagram size={40} color="white" />
+        </div>
+      ) : (
+        <div
+          className={`${style.linkTag} ${showLinkTag ? style.show : ''}`}
+        >
+          <FaFacebookSquare size={20} color="black" />
+          <AiFillGithub size={20} color="black" />
+          <AiFillInstagram size={20} color="black" />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+
+import style from "./Projects.module.css"
+const Projects = () => {
+  return (
+    <div className={style.container}>
+      
+    </div>
+  )
+}
+
+export default Projects
