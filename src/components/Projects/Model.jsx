12837e594f302313761d3456c500c180f0529c89
Milansenapati69 <milansenapati69@gmail.com>
2023-10-13 18:08:51 +0530

    push

--- src/components/Projects/Model.jsx
+++ src/components/Projects/Model.jsx
@@ -1,45 +1,49 @@
 import React, { useRef } from "react";
-import { useGLTF, Html,Plane } from "@react-three/drei";
+import { useGLTF, Html, Plane } from "@react-three/drei";
 import About from "../About/About";
 
 export default function Model(props) {
   const { nodes, materials } = useGLTF("./web.glb");
-  console.log(nodes)
-  
+  const isHovered=props.Hovered
+
+  // console.log(isHovered)
+
   return (
- 
-      <group {...props} dispose={null} scale={[2,2,2]}>
+
+    <group {...props} dispose={null} scale={[3, 3, 3]} Hovered>
       <mesh
-      castShadow
-      receiveShadow
-      geometry={nodes.Cube.geometry}
-      material={materials["Material.001"]}
-      position={[0, -1.443, 1.613]}
-      rotation={[0.371, 0, 0]}
-      scale={[2.3, 0.017, 0.614]}
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube.geometry}
+        material={materials["Material.001"]}
+        position={[0, -1.443, 1.613]}
+        rotation={[0.371, 0, 0]}
+        scale={[2.3, 0.017, 0.614]}
+      >
+
+      </mesh>
+      <Plane visible={false}>
+        <Html className="content" rotation-x={-Math.PI * 2} position={[0.0001, 0, 1]} transform scale={0.08} center style={{ opacity: "0.8", width: "4000px",height:"100%"}}
         >
-   
-        </mesh>
-        <Plane visible={false}>
-        <Html className="content" rotation-x={-Math.PI*2} position={[0.0001, 0, 1]} transform scale={0.08} center style={{opacity:"0.8",width:"4000px"}}
-        >
-        <div className="wrapper" onPointerDown={(e) => e.stopPropagation()}>
-          <About/>
-        </div>
-      </Html>
-        </Plane>
-        <mesh
+          <div style={{height:"100%"}} onPointerDown={(e) => e.stopPropagation()}>
+            {
+              isHovered 
+            }
+          </div>
+        </Html>
+      </Plane>
+      <mesh
         castShadow
         receiveShadow
         geometry={nodes.Cube001.geometry}
         material={materials["Material.003"]}
         rotation={[0.371, 0, 0]}
         scale={[2.3, 0.017, 0.614]}
-        >
-       
-        </mesh>
-      </group>
-    );
+      >
+
+      </mesh>
+    </group>
+  );
 }
 
 useGLTF.preload("./web.glb");
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,35 +1,66 @@
-import { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Environment, ContactShadows, PresentationControls } from '@react-three/drei';
 import Model from './Model';
 import style from './Projects.module.css';
-
+import About from "../About/About";
+import Service from "../Service/Service";
 
 const Projects = () => {
+  const [isHovered, setIsHovered] = useState('');
+
+  const nameComponentMap = {
+    MAGENTO: <About />,
+    REACT: <Service />,
+    ANGULAR: "<h1>ANGULAR</h1>",
+    PHP: "<h1>PHP</h1>",
+    LARAVEL: "<h1>LARAVEL</h1>",
+  };
+  const handleHover = (name) => {
+    setIsHovered(name);
+  };
+
+
+
+
+
   return (
     <div className={style.container}>
-      <Canvas
-        camera={{ position: [-10, 0, -25], fov: 55 }}
-        style={{ position: 'absolute', zIndex: 200 }}
-        size={{ width: window.innerWidth, height: window.innerHeight }}
-      >
-      
-        <PresentationControls
-          config={{ mass: 2, tension: 500 }}
-          snap={{ mass: 4, tension: 1500 }}
-          rotation={[0, 0.3, 0]}
-          polar={[-Math.PI / 3, Math.PI / 3]}
-          azimuth={[-Math.PI / 1.4, Math.PI / 2]}
+      <div className={style.listItem}>
+        {Object.keys(nameComponentMap).map((name) => (
+          <div
+            key={name}
+            onMouseEnter={() => handleHover(name)}
+            className={style.Item}
+          >
+            {name}
+          </div>
+        ))}
+      </div>
+
+      <div className={style.CanvaContainer}>
+        <Canvas
+          camera={{ position: [-10, 0, -25], fov: 55 }}
+          style={{ position: 'absolute', zIndex: 200 }}
+          size={{ width: window.innerWidth, height: window.innerHeight }}
         >
-          <group rotation={[0, Math.PI, 0]} position={[0, 1, 0]}>
-            <Suspense >
-              <Model />
-            </Suspense>
-          </group>
-        </PresentationControls>
-        <Environment preset="city" />
-        <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} />
-      </Canvas>
+          <PresentationControls
+            config={{ mass: 2, tension: 500 }}
+            snap={{ mass: 4, tension: 1500 }}
+            rotation={[0, 0.3, 0]}
+            polar={[-Math.PI / 3, Math.PI / 3]}
+            azimuth={[-Math.PI / 1.4, Math.PI / 2]}
+          >
+            <group rotation={[0, Math.PI, 0]} position={[0, 1, 0]}>
+              <Suspense>
+                <Model Hovered={nameComponentMap[isHovered]} />
+              </Suspense>
+            </group>
+          </PresentationControls>
+          <Environment preset="city" />
+          <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} />
+        </Canvas>
+      </div>
     </div>
   );
 };
