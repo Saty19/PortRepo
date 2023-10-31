87376ead2cc9a86fd8a71af4ef4ccaa4ed543578
Satyadarsan senapati <141367726+Saty19@users.noreply.github.com>
2023-11-01 02:49:35 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
   base: '/PortRepo/',
   plugins: [
     react(),
-   
+    reactRefresh(),
   ],
 })
